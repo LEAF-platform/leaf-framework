6db49fc36863ad8fb0b85c3e1ef340f943ad0042
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-10-08 08:56:50 +0200

    Overhaul, - simulated should be a plugin not a startup command? - Number of seconds between changes? of what? Should this not be on device level? - Debug mode added for debug logging, still need proper implementation - Config file added as argument with default config.yaml - Overall comments added as i was not sure what was doing what sometimes - Simulated code part "removed" - Always start()

--- start.py
+++ start.py
@@ -1,29 +1,50 @@
 import argparse
+import logging
+
 import yaml
+
 import register as register
 
-def parse_args():
+DEBUG = False
+
+logging.basicConfig(level=logging.INFO)
+
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="Proxy to monitor a supported bioreactor"
+        description="Proxy to monitor equipment and send data to the cloud."
     )
-    parser.add_argument(
-        '--simulated', 
-        type=str, 
-        default=None, 
-        help='Run in simulated mode and provide file to take data from.'
-    )                                       
-    parser.add_argument(
-        '-s', '--seconds', 
-        type=int, 
-        default=None, 
-        help='Number of seconds between changes (optional for Simulated mode).'
-    )
+    # parser.add_argument(
+    #     '--simulated',
+    #     type=str,
+    #     default=None,
+    #     help='Run in simulated mode and provide file to take data from.'
+    # )
+    # This should be at configuration level as each adapter can have different config
+    # parser.add_argument(
+    #     '-s', '--seconds',
+    #     type=int,
+    #     default=None,
+    #     help='Number of seconds between changes (optional for Simulated mode).'
+    # )
     parser.add_argument(
         '-d', '--delay', 
         type=int, 
-        default=None, 
-        help='A delay in seconds before the proxy begins.'
+        default=0,
+        help='A delay in seconds before the proxy begins.' # TODO is this needed?
     )
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Enable debug logging.'
+    )
+
+    parser.add_argument(
+        "-c", "--config",
+        type=str,
+        default="config.yaml",
+        help="The configuration file to use."
+    )
+
     return parser.parse_args()
 
 def _get_output_module(config):
@@ -31,7 +52,8 @@ def _get_output_module(config):
     output_objects = {}
     fallback_codes = set()
     for out_data in outputs:
-        output_code = out_data.pop("code")
+        logging.debug(f"Output data: {out_data}")
+        output_code = out_data.pop("plugin")
         fallback_code = out_data.pop("fallback", None)
         if fallback_code:
             fallback_codes.add(fallback_code)
@@ -51,37 +73,55 @@ def _get_output_module(config):
 
 
 def main():
-    with open('config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
-    delay = 0
+    logging.info("Starting the proxy.")
+    # Parse command line arguments
     args = parse_args()
-    if args.delay is not None:
-        delay = int(args.delay)
-    
+    # Set up logging
+    if args.debug:
+        logging.debug("Debug logging enabled.")
+        logging.basicConfig(level=logging.DEBUG)
+    # Load the configuration file
+    logging.debug(f"Loading configuration file: {args.config}")
+    with open(args.config, 'r') as file:
+        config = yaml.safe_load(file)
+    logging.info(f"Configuration: {args.config} loaded.")
+
+    # delay = 0
+    # if args.delay is not None:
+
+    # Obtain the delay from the command line arguments, TODO is this needed?
+    logging.debug(f"Delay: {args.delay} of type {type(args.delay)}")
+    # delay = int(args.delay)
+
+    # Get the output module, multiple outputs are supported
     output = _get_output_module(config)
 
+    # Get the equipment module TODO (AKA adapters?)
     equipment_requirements = config["EQUIPMENT"]
+    # TODO what is equipment code?
     equipment_code = equipment_requirements.pop("code")
+    # Adapters for the different equipment in your laboratory
     adapter = register.get_equipment_adapter(equipment_code)
     instance_data = config["EQUIPMENT_DATA"]
+
+    # Checking if the unique ID is already taken?
     if instance_data["instance_id"] in output.get_existing_ids():
         raise ValueError(f'ID: {instance_data["instance_id"]} is taken.')
     try:
-        adapter = adapter(instance_data,output,
-                          **equipment_requirements)
+        adapter = adapter(instance_data,output, **equipment_requirements)
     except ValueError as ex:
         print("Error:")
         print(ex)
-        exit(-1)
+        exit(-1) # TODO Exit with error code -1 is that correct?
 
-    if args.simulated is not None:
-        if not hasattr(adapter,"simulate"):
-            raise NotImplementedError(f'Adapter {equipment_code} doesnt have a simulator.')
-        print("Simulator started.")
-        adapter.simulate(args.simulated,args.seconds,delay)
-    else:
-        print("Proxy started.")
-        adapter.start()
+    # TODO This should be moved to the adapter
+    #     if not hasattr(adapter,"simulate"):
+    #         raise NotImplementedError(f'Adapter {equipment_code} doesnt have a simulator.')
+    #     print("Simulator started.")
+    #     adapter.simulate(args.simulated,args.seconds,delay)
+    # else:
+    #     print("Proxy started.")
+    adapter.start()
 
 if __name__ == "__main__":
     main()
