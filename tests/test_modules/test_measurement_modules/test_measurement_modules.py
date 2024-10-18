10c1f9402861c8971dc20730e7dcd32cceba4c1a
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-10-18 15:03:04 +0200

    config file was slightly different

--- tests/test_modules/test_measurement_modules/test_measurement_modules.py
+++ tests/test_modules/test_measurement_modules/test_measurement_modules.py
@@ -12,9 +12,10 @@ test_dir = os.path.dirname(os.path.abspath(__file__))
 
 with open(test_dir + '/../../test_config.yaml', 'r') as file:
     config = yaml.safe_load(file)
-broker = config["OUTPUT"]["broker_address"]
-port = int(config["OUTPUT"]["port"])
-un = config["OUTPUT"]["username"]
-pw = config["OUTPUT"]["password"]
+broker = config["OUTPUTS"][0]["broker"]
+port = int(config["OUTPUTS"][0]["port"])
+if "username" in config["OUTPUTS"][0]:
+    un = config["OUTPUTS"][0]["username"]
+    pw = config["OUTPUTS"][0]["password"]
 test_file_dir = "test_dir"
 test_file = os.path.join(test_file_dir,"ecoli-GFP-mCherry_inter.csv")
