ca2301d4e0dbe86d43101db526a73dae90f60690
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-10-18 14:58:22 +0200

    type setting for mock_mqtt_client

--- tests/mock_mqtt_client.py
+++ tests/mock_mqtt_client.py
@@ -1,17 +1,18 @@
 import re
 import json
+import paho.mqtt.client as mqtt
 from core.modules.output_modules.mqtt import MQTT
 
 class MockBioreactorClient(MQTT):
-    def __init__(self, broker_address, port=None, 
-                 username=None,password=None):
+    def __init__(self, broker_address: str, port: int=1883,
+                 username: str|None=None,password: str|None=None):
         super().__init__(broker_address, port, 
                          username=username,password=password,clientid=None)
         self.messages = {}
         self.num_msg = 0
         self.client.on_message = self.on_message
 
-    def on_message(self, client, userdata, msg):
+    def on_message(self, client: mqtt.Client, userdata: str, msg: str) -> None:
         topic = msg.topic
         try:
             payload = msg.payload.decode('utf-8')
@@ -33,11 +34,12 @@ class MockBioreactorClient(MQTT):
         self.num_msg += 1
 
 
-    def subscribe(self, topic):
+    def subscribe(self, topic: str) -> str:
         topic = topic.strip().replace(" ", "")
         topic = re.sub(r"\s+", "", topic)
         self.client.subscribe(topic)
         return topic
     
-    def unsubscribe(self,topic):
+    def unsubscribe(self,topic: str) -> str:
         self.client.unsubscribe(topic)
+        return topic
