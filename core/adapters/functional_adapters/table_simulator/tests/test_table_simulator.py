7c19dc9f152e284099c1d829ef6ea9466a2de0ac
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-10-18 09:57:03 +0200

    sync table simulator to fix conflict

--- core/adapters/functional_adapters/table_simulator/tests/test_table_simulator.py
+++ core/adapters/functional_adapters/table_simulator/tests/test_table_simulator.py
@@ -1,11 +1,14 @@
 import csv
+import gzip
 import logging
 import os
 import shutil
 import time
 import unittest
+from datetime import timedelta
+from gzip import GzipFile
 from threading import Thread
-from typing import Any
+from typing import List, TextIO, Optional, Union, IO, Any
 
 import yaml
 
@@ -15,7 +18,7 @@ from core.adapters.functional_adapters.table_simulator.table_simulator import (
 from core.adapters.functional_adapters.table_simulator.table_simulator import (
     TableSimulatorInterpreter,
 )
-from core.measurement_terms.manager import measurement_manager
+from core.modules.logger_modules.logger_utils import get_logger
 from core.modules.output_modules.mqtt import MQTT
 from mock_mqtt_client import MockBioreactorClient
 
@@ -24,17 +27,21 @@ curr_dir = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(curr_dir, "data", "indpensim.yaml"), "r") as file:
     config = yaml.safe_load(file)
 
+SEPARATOR: str = ","
+
 broker = config["OUTPUTS"][0]["broker"]
 port = int(config["OUTPUTS"][0]["port"])
 try:
     un = config["OUTPUTS"][0]["username"]
     pw = config["OUTPUTS"][0]["password"]
-except:
+except KeyError:
     un = None
     pw = None
 time_column = config["EQUIPMENT_INSTANCES"][0]["equipment"]["requirements"][
     "time_column"
 ]
+
+logger = get_logger(__name__, log_file="app.log", log_level=logging.DEBUG)
 
 watch_file: str = os.path.join("tmp.txt")
 test_file_dir: str = os.path.join(curr_dir, "data")
@@ -67,50 +74,133 @@ class TestTableSimulatorInterpreter(unittest.TestCase):
 
     def _metadata_run(self) -> dict[str, str]:
         with open(measurement_file, "r", encoding="latin-1") as file:
-            data = list(csv.reader(file, delimiter=";"))
+            data = list(csv.reader(file, delimiter=SEPARATOR))
         return self._interpreter.metadata(data)
 
     def test_metadata(self) -> None:
         result = self._metadata_run()
         self.assertIn("experiment_id", result)
 
+    # Returns a gzip file object or a normal file object
+    def smart_open(self, filepath: str, mode: str = "r") -> Union[IO[Any], GzipFile]:
+        """Opens the file with gzip if it ends in .gz, otherwise opens normally."""
+        if filepath.endswith(".gz"):
+            return gzip.open(filepath, mode)
+        else:
+            return open(filepath, mode)
+
     def test_measurement(self) -> None:
-        # TODO - Fix this test, it failes as the simulator fixes the time header...
-        #  but this function is not called in this test case
-        result = self._metadata_run()
-        measurement_terms = measurement_manager.get_measurements()
-        with open(measurement_file, "r", encoding="latin-1") as file:
-            data = list(csv.reader(file, delimiter=";"))
-        result = self._interpreter.measurement(data, None)
-        print(result)
+        self.instance_id = config["EQUIPMENT_INSTANCES"][0]["equipment"]["data"]["instance_id"]
+        self.institute = config["EQUIPMENT_INSTANCES"][0]["equipment"]["data"]["institute"]
+        self._write_file = config["EQUIPMENT_INSTANCES"][0]["equipment"]["requirements"]["write_file"]
+        self.time_column = config["EQUIPMENT_INSTANCES"][0]["equipment"]["requirements"]["time_column"]
+        self._start_datetime = config["EQUIPMENT_INSTANCES"][0]["equipment"]["requirements"]["start_date"]
+        self._filename = config["EQUIPMENT_INSTANCES"][0]["equipment"]["simulation"]["filename"]
+        self._interval = config["EQUIPMENT_INSTANCES"][0]["equipment"]["simulation"]["interval"]
 
-    def test_simulate(self) -> None:
-        pass
+        logger.info(
+            f"Simulating nothing yet for {self.instance_id} at {self.institute} with input file {self._filename} and interval of {self._interval} seconds"
+        )
 
+        filepath = measurement_file
+        with self.smart_open(filepath, "rb") as f:
+            for index, lineb in enumerate(f):
+                try:
+                    line_split: List[str] = (
+                        lineb.decode("utf-8").strip().split(SEPARATOR)
+                    )
+                    if index == 0:
+                        # Rename the time column to timestamp
+                        line_split[line_split.index(self.time_column)] = "timestamp"
+                        header = line_split
+                        # Checks if file exists and remove if needed
+                        if os.path.isfile(self._write_file):
+                            logger.warning(
+                                f"Trying to run test when the file exists at {self._write_file}"
+                            )
+                            # Remove the file
+                            os.remove(self._write_file)
+
+                    # Change the time column to a datetime object with the start date
+                    if index > 0:
+                        # Get the time
+                        try:
+                            time_index = header.index("timestamp")
+                            logger.debug(f"Time index: {time_index}")
+                        except:
+                            raise BaseException(
+                                f"Time column 'timestamp' not found in the header {header}"
+                            )
+                        time_value = line_split[time_index].strip("\"' ")
+                        # Check if the time value is a float / integer
+                        try:
+                            time_h = float(time_value)
+                            # Check if self._start_datetime is defined
+                            if hasattr(self, "_start_datetime"):
+                                # Convert to a datetime object
+                                new_time = self._start_datetime + timedelta(
+                                    hours=time_h
+                                )
+                                # Replace the time column
+                                line_split[time_index] = new_time.strftime(
+                                    "%Y-%m-%d %H:%M:%S"
+                                )
+                                logger.debug(
+                                    f"Time value: {time_value} and new time: {line_split[time_index]}"
+                                )
+                            else:
+                                logger.warning(
+                                    f"Start date not defined, using {line_split[time_index]} as the time"
+                                )
+                        except ValueError:
+                            logger.debug(f"Time value: {time_value} was not a digit")
+                            # Check if the time value is a datetime object
+                            time_obj = dateparser.parse(time_value)
+                            # Replace the time column
+                            line_split[time_index] = time_obj.strftime(
+                                "%Y-%m-%d %H:%M:%S"
+                            )
+                            logger.debug(
+                                f"Time value: {time_value} and time object: {time_obj} and new time: {line_split[time_index]}"
+                            )
+                    # Join the line
+                    lineb = f"{SEPARATOR}".join(line_split).encode("utf-8") + b"\n"
+                    # Write to
+                    with open(self._write_file, "ab") as write_file:
+                        write_file.write(lineb)
+                        logger.info(f"Writing line {index} to {self._write_file}")
+                except Exception as e:
+                    logger.error(f"Error in simulate: {e}")
+                time.sleep(self._interval)
+        # Finish the simulation
+        logger.info("Finished simulation")
 
 class TestTableSimulatorAdapter(unittest.TestCase):
-
-    def setUp(self):
+    def setUp(self) -> None:
         if os.path.isfile(watch_file):
             os.remove(watch_file)
 
         self.mock_client: MQTT = MockBioreactorClient(broker, port, username=un, password=pw)
         logging.debug(f"Broker: {broker} Port: {port} Username: {un}")
         self.output: MQTT = MQTT(broker, port, username=un, password=pw)
-        self.instance_data: dict = {
+        self.instance_data: dict[str, str] = {
             "instance_id": "test_IndPenSimAdapter",
             "institute": "test_ins",
         }
+
         self._adapter: TableSimulatorAdapter = TableSimulatorAdapter(
             self.instance_data, self.output, watch_file, time_column
         )
+
         self.details_topic = self._adapter._metadata_manager.details()
         self.start_topic = self._adapter._metadata_manager.experiment.start()
         self.stop_topic = self._adapter._metadata_manager.experiment.stop()
         self.running_topic = self._adapter._metadata_manager.running()
 
+        logger.debug(f"Details topic: {self.details_topic}")
         self._flush_topics()
         time.sleep(2)
+        logger.debug(f"Subscribing to {self.start_topic}")
         wildcard_measure = self._adapter._metadata_manager.experiment.measurement()
         self.mock_client.subscribe(self.start_topic)
         self.mock_client.subscribe(self.stop_topic)
@@ -124,7 +214,7 @@ class TestTableSimulatorAdapter(unittest.TestCase):
         self._flush_topics()
         self.mock_client.reset_messages()
 
-    def _get_measurements_run(self):
+    def _get_measurements_run(self) -> dict[str, str]:
         with open(measurement_file, "r", encoding="latin-1") as file:
             data = list(csv.reader(file, delimiter=";"))
         self._adapter._interpreter.metadata(data)
@@ -133,6 +223,7 @@ class TestTableSimulatorAdapter(unittest.TestCase):
         return self._adapter._interpreter.measurement(data)
 
     def _flush_topics(self) -> None:
+        logger.debug("Flushing topics")
         self.mock_client.flush(self.details_topic)
         self.mock_client.flush(self.start_topic)
         self.mock_client.flush(self.stop_topic)
