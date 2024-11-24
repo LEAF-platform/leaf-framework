fb294fe889e9d8d51f1acf1feab72a155ee82b84
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-11-24 15:21:48 +0000

    It is now able to find leaf adapters installed via pip and execute them using the config file.

---
+++ tests/test_adapters/test_hello_world.py
@@ -0,0 +1,30 @@
+# Test for a remote git repository with a hello world example
+import logging
+import os
+import unittest
+
+from leaf.modules.logger_modules.logger_utils import get_logger
+from leaf.modules.output_modules.mqtt import MQTT
+
+logger = get_logger(__name__, log_file="app.log", log_level=logging.DEBUG)
+
+class TestHelloWorld(unittest.TestCase):
+    def setUp(self) -> None:
+        logger.info("Clearing log file")
+        if os.path.exists("app.log"):
+            os.remove("app.log")
+
+    def test_hello_world_adapter(self) -> None:
+        self.output = MQTT("test.mosquitto.org", 1883)
+        # self.output.transmit("test", """'{"test": "test"}""")
+        self.instance_data: dict[str, str] = {
+            "instance_id": "test_maq",
+            "institute": "test_ins",
+            "experiment_id": "test_exp",
+        }
+
+        # Import the adapter
+        from hello_world.adapter import HelloWorldAdapter
+        adap = HelloWorldAdapter(output=self.output)
+        adap.start()
+        print("HelloWorldAdapter started successfully.")
