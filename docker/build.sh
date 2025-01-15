ca8cd9bcd6ca6a17c0ec831aeba607058a13eef3
Jasper Koehorst <jasper.koehorst@wur.nl>
2025-01-15 16:56:10 +0100

    simplified docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 docker login docker-registry.wur.nl
 
-docker build --platform linux/amd64 -t docker-registry.wur.nl/m-unlock/docker/leaf:dev .
+docker build --no-cache --platform linux/amd64 -t docker-registry.wur.nl/m-unlock/docker/leaf:dev .
 
 docker push docker-registry.wur.nl/m-unlock/docker/leaf:dev
--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Starting the LEAF program"
-# Starting the LEAF program
-ls /app
-# In the future, this will be part of the docker image
-if [ -f /app/requirements.txt ]; then
-    echo "Requirements file found"
-    cat /app/requirements.txt
-    pip install --no-cache-dir --force-reinstall -r /app/requirements.txt
-else
-    echo "No requirements.txt file found"
-fi
 
+# Starting the LEAF program
 leaf --config /app/config.yaml
