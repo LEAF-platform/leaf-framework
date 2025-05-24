b9e3bb13471505e52f515d9ef4442ac0ce254b3c
Jasper <jasper.koehorst@wur.nl>
2025-05-24 15:27:45 +0200

    to test a normal startup, disabled for gitlab runner

--- tests/test_run_local.py
+++ tests/test_run_local.py
@@ -4,7 +4,7 @@ import leaf.start
 
 
 # Create a test case but only local execution
-# @unittest.skip("Skipping test for local")
+@unittest.skip("Skipping test for local")
 class TestLocal(unittest.TestCase):
     def test_local(self) -> None:
         leaf.start.main([])
