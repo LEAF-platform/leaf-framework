cb7c0e6b61b3c3c16e998811445156c01580a7e0
Jasper Koehorst <jasper.koehorst@wur.nl>
2025-07-30 16:01:57 +0200

    Complex test of keydb first then mqtt starting and if all keydb messages has been processed stop test

--- tests/test_modules/test_output_modules/test_keydb.py
+++ tests/test_modules/test_output_modules/test_keydb.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import multiprocessing
 import os
 import threading
 import time
@@ -8,9 +9,15 @@ import redis
 import yaml
 
 import leaf.start
+from leaf.modules.output_modules.mqtt import MQTT
 from leaf.utility.logger.logger_utils import get_logger
 
+curr_dir: str = os.path.dirname(os.path.realpath(__file__))
 logger = get_logger(__name__, log_file="input_module.log", log_level=logging.DEBUG)
+
+def run_leaf(config):
+    logger.info("Starting LEAF process")
+    leaf.start.main(["--nogui", "--config", config])
 
 class TestKeyDB(unittest.TestCase):
 
@@ -48,7 +55,8 @@ class TestKeyDB(unittest.TestCase):
         self.assertIsNone(result2)
 
     def test_leaf(self):
-        config = "../../../tests/static_files/test_config_keydb.yaml"
+
+        config = os.path.join(os.path.dirname(__file__), "..","..","..","tests", "static_files", "test_config_keydb.yaml")
         # Load configuration from the YAML file
         if not os.path.exists(config):
             raise FileNotFoundError(f"Configuration file {config} not found.")
@@ -72,13 +80,6 @@ class TestKeyDB(unittest.TestCase):
             print("Failed to connect to KeyDB:", e)
             self.fail("Could not connect to KeyDB")
 
-        def run_leaf():
-            try:
-                print("Launching LEAF")
-                leaf.start.main(["--nogui", "--config", config])
-                print("LEAF exited normally")
-            except Exception as e:
-                print("LEAF crashed:", e)
         # Start the leaf application in a separate thread
         thread = threading.Thread(target=run_leaf, daemon=True)
         thread.start()
@@ -115,23 +116,12 @@ class TestKeyDB(unittest.TestCase):
 
 
     def test_keydb_switch_to_mqtt(self):
-        config = "../../../tests/static_files/test_config_keydb.yaml"
-        # Start the LEAF application with KeyDB output module
-        logger.info("Starting LEAF with KeyDB output module")
-        def run_leaf():
-            try:
-                logger.info("Launching LEAF")
-                leaf.start.main(["--nogui", "--config", config])
-                logger.info("LEAF exited normally")
-            except Exception as e:
-                logger.info("LEAF crashed:", e)
-        # Start the leaf application in a separate thread
-        thread = threading.Thread(target=run_leaf, daemon=True)
-        thread.start()
+        config = os.path.join(os.path.dirname(__file__), "..","..","..","tests", "static_files", "test_config_keydb.yaml")
 
-        # Load configuration from the YAML file
         if not os.path.exists(config):
             raise FileNotFoundError(f"Configuration file {config} not found.")
+
+        # Load configuration from the YAML file
         with open(config, 'r') as file:
             config_data = yaml.safe_load(file)
             logger.info("Configuration loaded:", config_data)
@@ -143,48 +133,105 @@ class TestKeyDB(unittest.TestCase):
         port = keydb_config.get('port')
         db = keydb_config.get('db')
 
+        keydb_client = redis.Redis(host=host, port=port, db=db)
+        # Delete all keys in KeyDB to start fresh
+        keydb_client.flushdb()
+        logger.info("Deleted all keys in KeyDB")
+
+        # Start the LEAF application with KeyDB output module
+        logger.info("Starting LEAF with KeyDB output module")
+        # Start the leaf application in a separate thread
+        p = multiprocessing.Process(target=run_leaf, args=(config,))
+        p.start()
+
         logger.info(f"Connecting to KeyDB at {host}:{port}, DB: {db}")
         # Connect to the KeyDB instance
-        try:
-            keydb_client = redis.Redis(host=host, port=port, db=db)
-            # Delete all keys in KeyDB to start fresh
-            keydb_client.flushdb()
-            logger.info("Flushed KeyDB database")
-            # Wait for the LEAF thread to start and populate KeyDB
-            while True:
-                # Check if the thread is still alive
-                if not thread.is_alive():
-                    logger.info("LEAF thread has finished")
+        keydb_client = redis.Redis(host=host, port=port, db=db)
+        # Wait for the LEAF thread to start and populate KeyDB
+        while True:
+            # Check if the thread is still alive
+            if not p.is_alive():
+                logger.info("LEAF thread has finished")
+                break
+            # Obtain data from KeyDB
+            keys = keydb_client.keys()
+            if len(keys) == 2:
+                assert keys[0] == b'example_hello_world_institute1/HelloWorld/example_hello_world_id1/experiment/undefined/measurement/bioreactor_example'
+                assert keys[1] == b'example_hello_world_institute1/HelloWorld/example_hello_world_id1/details'
+                logger.info("Current keys in KeyDB: %s", keys)
+                # Number of values in the list
+                size_now = keydb_client.llen(keys[0])
+                logger.info("Number of values in the list: %d", size_now)
+                if size_now > 5:
+                    # Stop the LEAF thread
+                    logger.info("Stopping LEAF thread due to sufficient data in KeyDB")
+                    p.join(1)
+                    p.terminate()
                     break
-                # Obtain data from KeyDB
-                keys = keydb_client.keys()
-                if len(keys) == 2:
-                    assert keys[0] == b'example_hello_world_institute1/HelloWorld/example_hello_world_id1/experiment/undefined/measurement/bioreactor_example'
-                    assert keys[1] == b'example_hello_world_institute1/HelloWorld/example_hello_world_id1/details'
-                    logger.info("Current keys in KeyDB: %s", keys)
-                    # Number of values in the list
-                    size_now = keydb_client.llen(keys[0])
-                    logger.info("Number of values in the list: %d", size_now)
-                    if size_now > 10:
-                        # Stop the LEAF thread
-                        logger.info("Stopping LEAF thread due to sufficient data in KeyDB")
-                        thread.join(1)
-                        break
-                # Sleep for a while to avoid busy waiting
-                time.sleep(1)
-            logger.info("Connected to KeyDB successfully")
-        except redis.ConnectionError as e:
-            logger.info("Failed to connect to KeyDB:", e)
-            self.fail("Could not connect to KeyDB")
+            # Sleep for a while to avoid busy waiting
+            time.sleep(1)
+
+        # Check if the LEAF process is still alive by counting the number of messages in KeyDB
+        while p.is_alive():
+            logger.info("Number of messages in KeyDB: %d", keydb_client.llen(keys[0]))
+            p.terminate()
+            p.join(1)
+
+        # Obtain all messages from KeyDB to check if they are sent over MQTT
+        keys = keydb_client.keys()
+        message_store = {}
+        for key in keys:
+            key = key.decode('utf-8')  # Decode bytes to string
+            message_store[key] = set()
+            logger.info("Key: %s", key)
+            messages = keydb_client.lrange(key, 0, -1)
+            for message in messages:
+                logger.info("Message: %s", message.decode('utf-8'))
+                message_store[key].add(json.loads(message.decode('utf-8')))
+
+        logger.info("Message store: %s", message_store)
 
         # Start LEAF in MQTT mode
         logger.info("Switching LEAF to MQTT mode")
-        config = "../../../tests/static_files/test_config_mqtt_with_keydb_fallback.yaml"
-        thread = threading.Thread(target=run_leaf, daemon=True)
-        thread.start()
+        config = os.path.join(os.path.dirname(__file__), "..", "..", "..", "tests", "static_files",
+                              "test_config_mqtt_with_keydb_fallback.yaml")
+
+        def on_message_test(client, userdata, msg):
+            """Callback function to handle incoming MQTT messages."""
+            logger.info(f"Received message on topic {msg.topic}: {msg.payload.decode('utf-8')}")
+            keys = message_store.keys()
+            if  msg.topic in keys:
+                for m in message_store[msg.topic]:
+                    if json.loads(msg.payload.decode('utf-8')) == m:
+                        logger.info("Message matches stored message, test passed")
+                        # Delete the message from message_store
+                        message_store[msg.topic].remove(m)
+                        logger.info(f"Number of remaining messages: {len(message_store[msg.topic])}")
+                        if len(message_store[msg.topic]) == 0:
+                            logger.info("All messages for topic %s have been received", msg.topic)
+                            del message_store[msg.topic]
+                        break
+
+        output = MQTT("localhost", 1883)
+        # output.client.connect("localhost", 1883, 60)
+        output.client.subscribe("#")
+        output.client.on_message = on_message_test
+
+        # run_leaf(config)
+
+        p = multiprocessing.Process(target=run_leaf, args=(config,))
+        p.start()
+
         # Wait for the thread to finish
-        while True:
+        while p.is_alive():
+            logger.info(f"Message store: {message_store}")
+            if len(message_store) == 0:
+                logger.info("All messages have been received, stopping LEAF")
+                p.terminate()
+                p.join(1)
+                break
             time.sleep(1)
+
 
 
 if __name__ == '__main__':
